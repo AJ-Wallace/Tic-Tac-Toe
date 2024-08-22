a3e4560e2d92f19779df2a0f7753f5d7c061723c
Andrew Wallace <ajwall1307@gmail.com>
2024-08-22 18:33:28 +0800

    Added Coordinate positions, added updating of the grid based on player selection

--- main.py
+++ main.py
@@ -1,23 +1,72 @@
+from time import sleep
+
 class grid():
 
     def __init__(self):
         self.grid = []
+        self.selector = ''
+        self.turn = 1
 
     def setGrid(self):
         for i in range(3):
             self.grid.append([' ',' ',' '])
 
     def dispGrid(self):
-   
-        print("   |   |   ")
-        print("", self.grid[0][0],"|", self.grid[0][1],"|", self.grid[0][2],"")
-        print("---|---|---")
-        print("", self.grid[1][0],"|", self.grid[1][1],"|", self.grid[1][2],"")
-        print("---|---|---")
-        print("", self.grid[2][0],"|", self.grid[2][1],"|", self.grid[2][2],"")
-        print("   |   |   ")
+
+        print("   1   2   3 ")
+        print("     |   |   ")
+        print("A ", self.grid[0][0],"|", self.grid[0][1],"|", self.grid[0][2],"")
+        print("  ---|---|---")
+        print("B ", self.grid[1][0],"|", self.grid[1][1],"|", self.grid[1][2],"")
+        print("  ---|---|---")
+        print("C ", self.grid[2][0],"|", self.grid[2][1],"|", self.grid[2][2],"")
+        print("     |   |   ")
+
+    def getMove(self):
+        validCoord = ["A3","A1",'A2','B3','B1','B2','C3','C1','C2']
+        coord = input("Select a position: ").upper()
+
+        while coord not in validCoord:
+            print("Invalid Coord", end='\r')
+            sleep(2)
+            coord = input("Select a position: ").upper() 
+
+        match coord:
+            case "A1":
+                self.grid[0][0] = self.selector
+            case "A2":
+                self.grid[0][1] = self.selector
+            case "A3":
+                self.grid[0][2] = self.selector
+            case "B1":
+                self.grid[1][0] = self.selector
+            case "B2":
+                self.grid[1][1] = self.selector
+            case "B3":
+                self.grid[1][2] = self.selector
+            case "C1":
+                self.grid[2][0] = self.selector
+            case "C2":
+                self.grid[2][1] = self.selector
+            case "C3":
+                self.grid[2][2] = self.selector
+
+
+    def switchPlayer(self,turn):
+        if self.turn % 2 == 0:
+            self.selector = 'O'
+        else:
+            self.selector = 'X'
+
 
 if __name__ == "__main__":
+    play = True
+
     grid = grid()
     grid.setGrid()
-    grid.dispGrid()
+
+    while play == True:
+        grid.dispGrid()
+        grid.switchPlayer(grid.turn)
+        grid.getMove()
+        grid.turn += 1
